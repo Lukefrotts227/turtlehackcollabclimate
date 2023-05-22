49dd8b6cae8729ff3677e3417abb00d5062f0a26
Lukas Frotton <132273525+Lukefrotts227@users.noreply.github.com>
2023-05-22 10:01:05 -0400

    Add files via upload

---
+++ front/my-app/src/App.js
@@ -0,0 +1,21 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import AuthContainer from './comp/authcontainer';
+import Basic from './comp/studentpage';
+
+function App( ) {
+  
+
+  return (
+    <div className = "main-app">
+      <>
+      <AuthContainer />
+      
+      </>
+    </div>
+  );
+}
+
+export default App;
